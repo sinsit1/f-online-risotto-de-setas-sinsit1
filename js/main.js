c89183f659aed5e3e70caa5761ffa234b3908e30
sinsit1 <alexandra8989@gmail.com>
2018-11-05 18:47:18 +0100

    add total and subtotal

--- js/main.js
+++ js/main.js
@@ -2,6 +2,16 @@
 
 const url ='https://raw.githubusercontent.com/Adalab/recipes-data/master/rissoto-setas.json';
 let result;
+let subtotalExpenses=0;
+let totalExpenses=0;
+
+    const form= document.querySelector('form');
+    const items_number = document.querySelector('.total__items');
+    const subtotal = document.querySelector('.subtotal__price');
+    const shippingCost = document.querySelector('.shipping__price');
+    const total = document.querySelector('.total__price');
+    const submit= document.createElement('input');
+
 
 fetch(url)
     .then(res => res.json())
@@ -9,13 +19,19 @@ fetch(url)
         result= data;
       console.log('data', result);
       list_products();
+      recipeName(result.recipe.name);
 });
+
+function recipeName(name){
+    const recipeName = document.querySelector('.recipe__name');
+    recipeName.innerHTML=name;
+}
 
 function list_products(){
     const product= result.recipe.ingredients;
+
     console.log(product);
-    const recipeName = document.querySelector('.recipe__name');
-        recipeName.innerHTML=result.recipe.name;
+
     for (var i = 0; i < product.length; i++) {
         const li = document.createElement('li');
         const check = document.createElement('input');
@@ -24,33 +40,101 @@ function list_products(){
         const price = document.createElement('p');
         const priceCurrency = document.createElement('h2');
         const quantityNumber = document.createElement ('input');
+
         article.innerHTML = product[i].product;
         priceCurrency.innerHTML = product[i].price+' '+result.recipe.currency;
-        brand.innerHTML = product[i].brand;
+
+        product[i].brand !== undefined 
+        ? brand.innerHTML = product[i].brand
+        : brand.innerHTML ='';
+
         price.innerHTML = product[i].price+' '+ product[i].quantity ;
+
         quantityNumber.type='number';
+        quantityNumber.min=0;
+        quantityNumber.value= product[i].items;
+        
         check.type='checkbox';
         check.name = 'products';
+        check.value = product[i].price;
         const list = document.querySelector('.form__products');
-        
-
-        
 
         list.appendChild(li);
         li.appendChild(check);
         li.appendChild(quantityNumber);
         li.appendChild(article);
-        li.appendChild(brand);
+        li.appendChild(brand === undefined ? '': brand);
         li.appendChild(price);
         li.appendChild(priceCurrency);
-
-      }
+    }
+    handlerCheck();
+    expenses();
 }
 
+
+
+function handlerCheck() {
+    let checkboxesList = document.querySelectorAll('input[name=products]');
+    checkboxesList.forEach((checkboxes) => {
+        console.log('holi');
+        return checkboxes.addEventListener('change', subtotal_calc);
+    })
+   
+}
+
+
+function expenses(){
+    const product= result.recipe.ingredients;
+
+    
+    
+
+    items_number.innerHTML= 'Items: '+product.length;
+    subtotal.innerHTML= 'Subtotal: '+subtotalExpenses;
+    shippingCost.innerHTML='Shipping Cost: 7'+result.recipe.currency;
+    total.innerHTML='Total: '+totalExpenses+shippingCost;
+    submit.type='submit';
+    submit.value= 'Comprar ingredientes total: '+totalExpenses;
+    form.appendChild(items_number);
+    form.appendChild(subtotal);
+    form.appendChild(shippingCost);
+    form.appendChild(total);
+    form.appendChild(submit);
+
+}
+
+function subtotal_calc(){
+
+    let money = this.value;
+    parseInt(money);
+    parseInt(totalExpenses);
+    parseInt(subtotalExpenses);
+    console.log('holi',money )
+    
+    if (this.checked) {
+        subtotalExpenses = money;
+        
+        subtotal.innerHTML = 'Subtotal: ' +totalExpenses;
+        shippingCost.innerHTML = 'Gastos de envio: 7' + result.recipe.currency;
+        total.innerHTML = 'Total: ' +parseInt(totalExpenses) + (money + 7) + result.recipe.currency;
+        submit.innerHTML = 'Comprar ingredientes: ' + (totalExpenses + 7 ) + result.recipe.currency;
+    } else if (!this.checked) {
+        subtotalExpenses = money;
+        subtotal.innerHTML = 'Subtotal:' + (totalExpenses);
+        shippingCost.innerHTML = 'Gastos de envio: ' + 7 + result.recipe.currency;
+        total.innerHTML = 'Total: ' + (totalExpenses + 7) + result.recipe.currency;
+        submit.innerHTML = 'Comprar ingredientes: ' + (totalExpenses + 7) + result.recipe.currency;
+    }    
+    
+} 
+
+
+
+
 function select_all(){ 
-    for (let i=0;i<document.form.elements.length;i++) 
+    for (let i=0;i<document.form.elements.length;i++){
        if(document.form.elements[i].type == "checkbox")	
-          document.form.elements[i].checked=1 
+          {document.form.elements[i].checked=1} }
  } 
 
  function deselect_all(){ 
